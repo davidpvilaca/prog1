16dfec4b2148883e92bbcd08a692adbd48ed13d8
davidpvilaca <davidpvilaca@hotmail.com>
2014-12-08 12:25:38 -0200

    Create ex4.py

---
+++ listas/lista2/ex4.py
@@ -0,0 +1,33 @@
+__author__ = 'David_Tadeu'
+
+
+def main():
+    #variaveis
+    l1 = []; l2 = []; l3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; i = 0; aux1=0; aux2=-1;
+
+    #entrada de dados
+    for i in range(10):
+        l1.append(int(input('Insira um numero para lista1: ')))
+    for i in range(10):
+        l2.append(int(input('Insira um numero para lista2: ')))
+
+    #processamento
+    for i in range(20): #loop com i sendo valores de 0 a 19, ou seja, ira repetir o loop 20 vezes
+        if i%2 == 0: #se i for par (resto da divisao por 2 = 0)
+            l3[i]=l1[aux1] #a lista l3 na posicao do i vai receber o equivalente da lista l1
+            aux1 += 1 #contador para lista l1
+        elif i%2 != 0: #se i for impar
+            l3[i]=l2[aux2] #a lista l3 vai receber, na posicao que a variavel i indicar, o equivalente da lista l1
+            aux2 += 1 #contador da lista l2
+
+
+    #saida de dados
+    print(l3) #imprimindo l3 na tela
+
+
+    return 0
+#--fim main--
+
+if __name__ == '__main__':
+    main()
+#--fim if--
